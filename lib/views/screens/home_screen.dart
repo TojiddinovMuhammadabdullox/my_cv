8ef20330c613ac00fb1ca9bef79db6947cf4e2e2
TojiddinovMuhammadabdullox <rahmatillooyatillo9@gmail.com>
2024-08-01 15:50:51 +0500

    sdfssdf

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:http/http.dart' as http;
+import 'package:permission_handler/permission_handler.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:my_cv/views/screens/academic_views.dart';
 import 'package:my_cv/views/screens/experience_views.dart';
 import 'package:my_cv/views/screens/home_views.dart';
@@ -7,7 +12,7 @@ import 'package:my_cv/views/screens/skills_views.dart';
 import 'package:my_cv/views/screens/summary_screen.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
@@ -15,6 +20,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 0;
+  bool _isLoading = false;
 
   static final List<Widget> _widgetOptions = <Widget>[
     const HomeContent(),
@@ -29,6 +35,86 @@ class _HomeScreenState extends State<HomeScreen> {
     setState(() {
       _selectedIndex = index;
     });
+  }
+
+  Future<bool> _requestStoragePermission() async {
+    if (Platform.isAndroid) {
+      if (await _isAndroid11OrHigher()) {
+        // For Android 11 (API 30) and above
+        if (!await Permission.manageExternalStorage.isGranted) {
+          final status = await Permission.manageExternalStorage.request();
+          return status.isGranted;
+        }
+      } else {
+        // For Android 10 and below
+        if (!await Permission.storage.isGranted) {
+          final status = await Permission.storage.request();
+          return status.isGranted;
+        }
+      }
+    }
+    return true; // Permission granted for iOS or already granted for Android
+  }
+
+  Future<bool> _isAndroid11OrHigher() async {
+    if (Platform.isAndroid) {
+      final androidInfo = await DeviceInfoPlugin().androidInfo;
+      return androidInfo.version.sdkInt >= 30;
+    }
+    return false;
+  }
+
+  Future<void> _downloadFile() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      final hasPermission = await _requestStoragePermission();
+      if (hasPermission) {
+        await _proceedWithDownload();
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+              content: Text('Storage permission denied. Unable to download.')),
+        );
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error: ${e.toString()}')),
+      );
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  Future<void> _proceedWithDownload() async {
+    final url =
+        'https://pub-7be1d45c4a744f86846c80e90df909eb.r2.dev/files/d2744b1d-45b5-4958-924f-6dd49b150e7f.pdf';
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      Directory? directory;
+      if (Platform.isAndroid) {
+        directory = await getExternalStorageDirectory();
+      } else if (Platform.isIOS) {
+        directory = await getApplicationDocumentsDirectory();
+      }
+
+      if (directory != null) {
+        final file = File('${directory.path}/cv.pdf');
+        await file.writeAsBytes(response.bodyBytes);
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Download completed: ${file.path}')),
+        );
+      } else {
+        throw Exception('Unable to access storage directory');
+      }
+    } else {
+      throw Exception('Failed to download file: ${response.statusCode}');
+    }
   }
 
   @override
@@ -49,8 +135,10 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         actions: [
           IconButton(
-            icon: const Icon(Icons.picture_as_pdf, color: Colors.black),
-            onPressed: () {},
+            icon: _isLoading
+                ? CircularProgressIndicator(color: Colors.black)
+                : const Icon(Icons.picture_as_pdf, color: Colors.black),
+            onPressed: _isLoading ? null : _downloadFile,
           ),
         ],
       ),
@@ -76,7 +164,8 @@ class _HomeScreenState extends State<HomeScreen> {
                     _buildDrawerItem('TECH SKILLS', 4),
                     _buildDrawerItem('LANGUAGES', 5),
                     _buildDrawerItem('PORTFOLIO', null, bold: true),
-                    _buildDrawerItem('DOWNLOAD CV', null, bold: true),
+                    _buildDrawerItem('DOWNLOAD CV', null,
+                        bold: true, onTap: _downloadFile),
                     _buildDrawerItem('CONTACT', null),
                   ],
                 ),
@@ -123,7 +212,8 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  Widget _buildDrawerItem(String title, int? index, {bool bold = false}) {
+  Widget _buildDrawerItem(String title, int? index,
+      {bool bold = false, VoidCallback? onTap}) {
     return Container(
       color: Colors.blue,
       child: ListTile(
@@ -134,7 +224,7 @@ class _HomeScreenState extends State<HomeScreen> {
             fontWeight: bold ? FontWeight.bold : FontWeight.normal,
           ),
         ),
-        onTap: index != null ? () => _onItemTapped(index) : null,
+        onTap: onTap ?? (index != null ? () => _onItemTapped(index) : null),
       ),
     );
   }
--- lib/views/widgets/timeline_item.dart
+++ lib/views/widgets/timeline_item.dart
@@ -7,8 +7,11 @@ class TimelineItem extends StatelessWidget {
   final bool isFirst;
   final bool isLast;
 
-  TimelineItem(
-      {required this.experience, this.isFirst = false, this.isLast = false});
+  const TimelineItem(
+      {super.key,
+      required this.experience,
+      this.isFirst = false,
+      this.isLast = false});
 
   @override
   Widget build(BuildContext context) {
@@ -24,12 +27,12 @@ class TimelineItem extends StatelessWidget {
                 color: Colors.pink,
               ),
             Container(
-              padding: EdgeInsets.all(8.0),
-              decoration: BoxDecoration(
+              padding: const EdgeInsets.all(8.0),
+              decoration: const BoxDecoration(
                 color: Colors.pink,
                 shape: BoxShape.circle,
               ),
-              child: Icon(Icons.circle, color: Colors.white, size: 16.0),
+              child: const Icon(Icons.circle, color: Colors.white, size: 16.0),
             ),
             if (!isLast)
               Container(
@@ -39,7 +42,7 @@ class TimelineItem extends StatelessWidget {
               ),
           ],
         ),
-        SizedBox(width: 16.0),
+        const SizedBox(width: 16.0),
         Expanded(
           child: ExperienceCard(experience: experience),
         ),
